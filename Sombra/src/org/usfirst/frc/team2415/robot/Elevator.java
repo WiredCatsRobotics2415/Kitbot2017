561484544123976dfbb62d5dfe6b7dc28f9285c2
westm <westm@DESKTOP-7Q6NH7C.westminster.net>
2018-02-10 17:21:19 -0500

    Removed solenoids part 2 (?)

--- Sombra/src/org/usfirst/frc/team2415/robot/Elevator.java
+++ Sombra/src/org/usfirst/frc/team2415/robot/Elevator.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class Elevator extends Subsystem {
+public class Elevator extends Subsystem {  
 
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
